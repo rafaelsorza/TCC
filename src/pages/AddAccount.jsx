44134371b5be2cd4848b68f02ab27b3617d886a6
git config --global user.email joaopr0701@gmail.com <joaopr0701@gmail.com>
2024-07-22 08:09:53 -0300

    adiçãonova

---
+++ src/pages/AddAccount.jsx
@@ -0,0 +1,15 @@
+import './AddAccount.css'; // Importe o arquivo CSS
+import React from 'react';
+
+function TelaConta() {
+   return (
+    <div className='container'>
+        <div className='caixas'>
+            <div className='caixa'> conteudo </div>
+            <div className='caixa'> conteudo </div>
+            </div>
+    </div>
+
+  );
+}
+export default TelaConta;
